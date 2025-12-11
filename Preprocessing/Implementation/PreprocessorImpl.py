750bce3af3733b93d59f7f49c2e07510cbb00f64
NinoGenco03 <133582911+NinoGenco03@users.noreply.github.com>
2025-12-11 12:16:00 +0100

    Implementazione PreprocessorImpl

--- Preprocessing/Implementation/PreprocessorImpl.py
+++ Preprocessing/Implementation/PreprocessorImpl.py
@@ -12,3 +12,28 @@ from Preprocessing.Preprocessor import Preprocessor
    - rimozione ID
    - separazione X e y. """
 
+class PreprocessorImpl(Preprocessor):
+
+    def preprocess(self, data_path: str):
+
+        df = pd.read_csv(data_path, dtype=str)
+        # dtype=str → necessario per gestire valori europei tipo "3,14"
+
+        first_col = df.columns[0].lower()
+        if "id" in first_col or "sample" in first_col:
+            df = df.drop(columns=[df.columns[0]])
+
+        df = df.drop_duplicates()
+
+        df = df.apply(lambda col: col.str.replace(",", ".", regex=False))
+
+        df = df.replace(["?", "", " ", "NaN", "nan"], np.nan)
+
+        df = df.astype(float)
+
+        df = df.fillna(df.mean())
+
+        X = df.iloc[:, :-1]
+        y = df.iloc[:, -1]
+
+        return X, y
