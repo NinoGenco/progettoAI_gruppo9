05b62f9d5d8f24d8ad09bb6aca4460890ce26863
andreanoce <andrea.nocera@alcampus.it>
2026-02-24 12:29:21 +0100

    Aggiornamento dei commenti in DistanceStrategy.py

--- KNNAlgorithm/CalculateDistance/Strategy/DistanceStrategy.py
+++ KNNAlgorithm/CalculateDistance/Strategy/DistanceStrategy.py
@@ -3,17 +3,18 @@ import numpy as np
 
 class DistanceStrategy(ABC):
 
-    """ Questa classe astratta definisce una modalità standard per tutte le strategie di calcolo della distanza.
-    Impone alle sottoclassi di implementare il metodo di calcolo specifico."""
+    """ Questa classe astratta funge da interfaccia per il Design Pattern 'Strategy', definendo una modalità standard che
+    tutte le strategie di calcolo della distanza dovranno rispettare e garantenfo che ogni metrica implementi il proprio metodo."""
 
     @abstractmethod
     def calculate(self, x1: np.ndarray, x2: np.ndarray) -> float:
 
         """ Calcola la distanza tra due vettori n-dimensionali.
 
-        Parametri: x1: Primo vettore numerico (Solitamente un campione del Training Set).
-                   x2: Secondo vettore numerico (Solitamente il punto di Test Set da classificare).
+        Parametri:
+                  x1 -> Primo vettore numerico (Solitamente un campione estratto dal Training Set).
+                  x2 -> Secondo vettore numerico (Solitamente un singolo campione estratto dal Test Set da classificare).
 
-        Risultati: Valore numerico float che rappresenta la distanza calcolata tra x1 e x2."""
+        Risultati -> Valore numerico float che rappresenta la distanza calcolata tra x1 e x2."""
 
         pass
